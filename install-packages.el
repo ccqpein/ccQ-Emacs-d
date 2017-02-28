f04a7e0d23f40b6f40783424159d55bf9183d787
ccQpein <ccqpein@protonmail.com>
2017-02-28 15:28:38 -0500

    add a bit comments

--- install-packages.el
+++ install-packages.el
@@ -1,22 +1,26 @@
 #! emacs --script 
 
+;;; make path var
 (defvar cpath "~/.emacs.d/")
 
 (unless (file-exists-p (concat cpath "elpa"))
   (make-directory (concat cpath "elpa")))
 
+;;; load package module
 (require 'package)
 (dolist (url '(("melpa" . "https://melpa.org/packages/")
 	       ("melpa-stable" . "http://stable.melpa.org/packages/")
 	       ))
   (add-to-list 'package-archives url t))
-
 (package-initialize)
 
+
+;;; read package list from `packages` file
 (defvar *packages-list (with-temp-buffer
                          (insert-file-contents (concat cpath "packages"))
                          (split-string (buffer-string) "\n" t)))
 
+;;; install packages
 (dolist (package *packages-list)
   (if (package-installed-p (intern package))
       (princ (format "%s already installed \n" package))
