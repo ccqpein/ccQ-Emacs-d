9027091990cd0308113b7b2f34f86a01a974e5ec
ccQ <ccqpein@protonmail.com>
2018-09-10 13:03:20 -0400

    use with-eval-after-load, next time try use-package

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -1,23 +1,24 @@
 ;;;;;;;; treemacs ;;;;;;;;;
 (require 'treemacs-mode)
-(setq treemacs-follow-after-init          t
-      treemacs-width                      25
-      treemacs-indentation                2
 
-      treemacs-git-integration            t
-      treemacs-git-mode                   'extended
-      treemacs-filewatch-mode             t
+(with-eval-after-load 'treemacs-mode
+ (setq treemacs-follow-after-init          t
+       treemacs-width                      25
+       treemacs-indentation                2
+
+       treemacs-git-integration            t
+       treemacs-git-mode                   'extended
+       treemacs-filewatch-mode             t
       
-      treemacs-collapse-dirs              3
-      treemacs-silent-refresh             nil
-      treemacs-change-root-without-asking nil
-      treemacs-sorting                    'alphabetic-desc
-      treemacs-show-hidden-files          t
-      treemacs-never-persist              nil
-      treemacs-is-never-other-window      nil
-      treemacs-goto-tag-strategy          'refetch-index
-      
-)
+       treemacs-collapse-dirs              3
+       treemacs-silent-refresh             nil
+       treemacs-change-root-without-asking nil
+       treemacs-sorting                    'alphabetic-desc
+       treemacs-show-hidden-files          t
+       treemacs-never-persist              nil
+       treemacs-is-never-other-window      nil
+       treemacs-goto-tag-strategy          'refetch-index
+       ))
 
 (global-set-key (kbd "C-x M-d") 'treemacs)
 (define-key treemacs-mode-map (kbd "M-p") 'treemacs-previous-project)
