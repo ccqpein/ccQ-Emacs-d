f5e279ae4836cdd048b6dd1dce1a48bf96c55467
ccQpein <ccqpein@protonmail.com>
2018-09-13 15:31:09 -0400

    finish treemacs mode

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -1,59 +1,31 @@
 ;;;;;;;; treemacs ;;;;;;;;;
-;(use-package treemacs
-;  ;:interpreter ("treemacs" . treemacs-mode)
-;
-;  :ensure t
-;  
-;  :init
-;  (bind-key "C-x M-d" treemacs)
-;
-;  :custom
-;  (treemacs-follow-after-init          t)
-;  (treemacs-width                      25)
-;  (treemacs-indentation                2)
-;  
-;  (treemacs-git-integration)            t
-;  (treemacs-git-mode                   'extended)
-;  (treemacs-filewatch-mode             t)
-;  
-;  (treemacs-collapse-dirs)              3
-;  (treemacs-silent-refresh             nil)
-;  (treemacs-change-root-without-asking nil)
-;  (treemacs-sorting                    'alphabetic-desc)
-;  (treemacs-show-hidden-files          t)
-;  (treemacs-never-persist              nil)
-;  (treemacs-is-never-other-window      nil)
-;  (treemacs-goto-tag-strategy          'refetch-index)
-;  
-;  :bind
-;  (:map treemacs-mode-map
-;   ("M-p" . treemacs-previous-project)
-;   ("M-n" . treemacs-next-project)
-;   ))
+(use-package treemacs
+  :ensure t
 
-(require 'treemacs-mode)
-
-(with-eval-after-load 'treemacs-mode
-(setq treemacs-follow-after-init          t
-      treemacs-width                      25
-      treemacs-indentation                2
-
-       treemacs-git-integration            t
-       treemacs-git-mode                   'extended
-       treemacs-filewatch-mode             t
-      
-       treemacs-collapse-dirs              3
-       treemacs-silent-refresh             nil
-       treemacs-change-root-without-asking nil
-       treemacs-sorting                    'alphabetic-desc
-       treemacs-show-hidden-files          t
-       treemacs-never-persist              nil
-       treemacs-is-never-other-window      nil
-       treemacs-goto-tag-strategy          'refetch-index
-       ))
-
-(global-set-key (kbd "C-x M-d") 'treemacs)
-(define-key treemacs-mode-map (kbd "M-p") 'treemacs-previous-project)
-(define-key treemacs-mode-map (kbd "M-n") 'treemacs-next-project)
+  :init (bind-key "C-x M-d" #'treemacs)
+  
+  :bind
+  (:map treemacs-mode-map
+   ("M-p" . treemacs-previous-project)
+   ("M-n" . treemacs-next-project))
+  
+  :custom
+  (treemacs-follow-after-init          t)
+  (treemacs-width                      25)
+  (treemacs-indentation                2)
+  
+  (treemacs-git-integration            t)
+  (treemacs-git-mode                   'extended)
+  (treemacs-filewatch-mode             t)
+  
+  (treemacs-collapse-dirs              3)
+  (treemacs-silent-refresh             nil)
+  (treemacs-change-root-without-asking nil)
+  (treemacs-sorting                    'alphabetic-desc)
+  (treemacs-show-hidden-files          t)
+  (treemacs-never-persist              nil)
+  (treemacs-is-never-other-window      nil)
+  (treemacs-goto-tag-strategy          'refetch-index)
+)
 
 (provide 'init-treemacs)
