471fd9b1d49f918cf860a1a768d5a3e8c2ab29ea
ccQ <ccqpein@protonmail.com>
2018-10-17 08:11:23 -0400

    add yaml mode

--- init.el
+++ init.el
@@ -142,6 +142,8 @@
 
 (require 'init-rust)
 
+(require 'init-yaml)
+
 
 ;;;;;;;;;;; custom setting;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
@@ -162,7 +164,7 @@
  '(markdown-open-command "~/.emacs.d/plugins/open-markdown-marked2.sh")
  '(package-selected-packages
    (quote
-	(helm-swoop use-package restclient treemacs flycheck-rust lsp-mode lsp-rust racer rust-mode highlight-symbol monokai-theme ensime tide helm-gtags helm ac-cider cider clojure-mode auctex magit neotree js2-mode web-mode swiper swift-mode slime py-autopep8 pkg-info paredit multiple-cursors markdown-mode jdee hindent haskell-mode google-this go-mode expand-region exec-path-from-shell elpy dash-at-point better-defaults ac-php)))
+	(yaml-mode helm-swoop use-package restclient treemacs flycheck-rust lsp-mode lsp-rust racer rust-mode highlight-symbol monokai-theme ensime tide helm-gtags helm ac-cider cider clojure-mode auctex magit neotree js2-mode web-mode swiper swift-mode slime py-autopep8 pkg-info paredit multiple-cursors markdown-mode jdee hindent haskell-mode google-this go-mode expand-region exec-path-from-shell elpy dash-at-point better-defaults ac-php)))
  '(recentf-max-saved-items 10)
  '(visible-bell nil))
 (custom-set-faces
---
+++ lisp/init-yaml.el
@@ -0,0 +1,6 @@
+(use-package yaml-mode
+  )
+
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+(provide 'init-yaml)
