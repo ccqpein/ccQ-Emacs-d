cc957f2ce014dac37c67034c4c99854e3a9e2609
ccQpein <ccqpein@protonmail.com>
2019-05-09 08:26:22 -0400

    give install from source script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# cd emacs source code and run this 
+brew update && brew install autoconf automake texinfo gnutls pkg-config libxml2
+
+export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
+
+./autogen.sh
+
+./configure && make && make install
+
+open ./nextstep
