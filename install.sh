9cca1c8455fedb7b98a7200e1afc91695788601e
ccQpein <ccqpein@protonmail.com>
2019-06-22 11:30:27 -0400

    update install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 brew update && brew install autoconf automake texinfo gnutls pkg-config libxml2
 
 export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/opt/libxml2/lib"
+export CPPFLAGS="-I/usr/local/opt/libxml2/include"
 
 ./autogen.sh
 
-./configure && make && make install
-
-open ./nextstep
+./configure && make && make install && open ./nextstep
