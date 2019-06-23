3dd4aaa58c5e7b7266e48b1ebdf4f30f0520b135
ccQpein <ccqpein@protonmail.com>
2019-06-22 22:21:27 -0400

    looks like it is not script issue

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 brew update && brew install autoconf automake texinfo gnutls pkg-config libxml2
 
 export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
-export LDFLAGS="-L/usr/local/opt/libxml2/lib"
-export CPPFLAGS="-I/usr/local/opt/libxml2/include"
+#export LDFLAGS="-L/usr/local/opt/libxml2/lib"
+#export CPPFLAGS="-I/usr/local/opt/libxml2/include"
 
 ./autogen.sh
 
