0af015962da5a25ca9a55819a19cc9b5b96f5d86
ccQpein <ccqpein@protonmail.com>
2019-09-25 16:20:11 -0400

    fix

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,10 +1,24 @@
 ;;;;;;;; Javascript mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(add-hook 'js2-mode-hook #'setup-tide-mode) ; for typescript mode
+(use-package js-mode
+  :hook
+  (
+   (js-mode . hs-minor-mode)
+   (js-mode . paredit-mode)
+   (js-mode . js2-minor-mode)
+   )
+  
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+  )
 
-(add-hook 'js-mode-hook 'paredit-mode-hook)
-(add-hook 'js-mode-hook 'hs-minor-mode-hook)
+
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+;; (add-hook 'js-mode-hook 'js2-minor-mode)
+;; (add-hook 'js2-mode-hook #'setup-tide-mode) ; for typescript mode
+
+;; (add-hook 'js-mode-hook 'paredit-mode)
+;; (add-hook 'js-mode-hook 'hs-minor-mode)
 
 (provide 'init-js)
