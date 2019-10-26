62a6915fd3af60af72136f076ceae55ff46bd303
ccQpein <ccqpein@protonmail.com>
2019-10-25 20:58:15 -0400

    rust does not need lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,22 +1,33 @@
 (use-package lsp-mode
   :hook
   ((go-mode . lsp-deferred)
-   ;;(rust-mode . lsp-deferred) ;; don't start rust auto
-   ) 
+   )
   :commands (lsp lsp-deferred)
-
-  :config
-  (require 'ra-emacs-lsp) ;; for rust https://github.com/rust-analyzer/rust-analyzer
   )
 
 (use-package lsp-ui
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+
+  :config
+  (setq lsp-ui-sideline-enable t
+        lsp-ui-doc-enable t
+        lsp-ui-flycheck-enable t
+        lsp-ui-imenu-enable t
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-log-io t)
+  )
+
 
 (use-package company-lsp
   :commands company-lsp)
 
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
 
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package helm-lsp
+  :commands helm-lsp-workspace-symbol)
+
+
+(use-package lsp-treemacs
+  :commands lsp-treemacs-errors-list)
+
 
 (provide 'init-lsp)
