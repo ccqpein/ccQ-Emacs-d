681d9b0495a09c59ab266f47379e789a01b2a375
ccQpein <ccqpein@protonmail.com>
2019-11-26 20:54:03 -0500

    change make to gmake

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 # cd emacs source code and run this 
 brew update && brew install autoconf automake texinfo gnutls pkg-config libxml2
 
-export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
-#export LDFLAGS="-L/usr/local/opt/libxml2/lib"
-#export CPPFLAGS="-I/usr/local/opt/libxml2/include"
+ export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
 
 ./autogen.sh
 
-./configure && make && make install && open ./nextstep
+./configure && gmake && gmake install && open ./nextstep
