0b5e28ff32532ca17c226cfc153f4a69a6476314
ccQpein <ccqpein@protonmail.com>
2020-02-26 20:21:29 -0500

    fix script

--- plugins/open-markdown-to-web.sh
+++ plugins/open-markdown-to-web.sh
@@ -1,1 +1,1 @@
-pandoc -f markdown -t html5 --css=/Users/ccQ/Library/Application\ Support/MacDown/Styles/GitHub2.css -s $1
+pandoc -f markdown -t html5 --css=/Applications/Marked\ 2.app/Contents/Resources/github.css -s $1
