61654ba285f007984ff5c9b9b1f49377e07ee832
ccQpein <ccqpein@protonmail.com>
2020-04-23 20:37:44 -0400

    write a script to hekp me find if rustanalyzer updated

---
+++ plugins/update-rust-analyzer.lisp
@@ -0,0 +1,30 @@
+(ql:quickload '("dexador" "lquery" "quri" "str"))
+
+(let* ((current-version (read-line (sb-ext:process-output
+                                    (sb-ext:run-program "rust-analyzer"
+                                                        '("--version")
+                                                        :search t
+                                                        :output :stream))))
+       (version-hash (subseq current-version (1+ (position #\Space current-version))))
+       
+       whole-page statue-code head uri stream release-page newest-hash 
+       )
+  
+  (multiple-value-setq (whole-page statue-code head uri stream)
+    (dex:get "https://github.com/rust-analyzer/rust-analyzer/releases/latest"))
+  
+  (setf release-page (lquery:$ (lquery:initialize whole-page))
+        newest-hash (elt (lquery:$ release-page "code" (text)) 0))
+  
+  ;;(format t "newest: ~a, current: ~a~%" newest-hash version-hash)
+  (if (string= newest-hash version-hash)
+      (format t "newest version ~a equal current version ~a, don't need to update~%"
+              newest-hash current-version)
+      (let ((data-tag (car (last (str:split-omit-nulls #\/ (quri:uri-path uri)))))
+            download-link)
+        (setf download-link (format nil
+                                    "https://github.com/rust-analyzer/rust-analyzer/releases/download/~a/rust-analyzer-mac"
+                                    data-tag))
+        (format t
+                "update rust-analyzer-mac by \"wget ~a -O ~~/.cargo/bin/rust-analyzer && chmod +x ~~/.cargo/bin/rust-analyzer\"~%"
+                download-link))))
