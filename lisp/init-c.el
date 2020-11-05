97566dc2e5af59a521dfc5e836e50abaaea045cc
ccQpein <ccqpein@protonmail.com>
2020-11-05 00:18:55 -0500

    cpp mode

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -4,10 +4,14 @@
 
   :config
   (setq clang-format-style "file"
-        clang-format-fallback-style "google")
+        clang-format-fallback-style "google"
+        flycheck-c/c++-gcc-executable "gcc-10"
+        flycheck-gcc-include-path '("/usr/include"
+                                    "/usr/local/Cellar/gcc/10.2.0/lib/gcc/10/gcc/x86_64-apple-darwin19/10.2.0/include/")
+        )
   (add-hook 'before-save-hook
             (lambda ()
-              (when (member major-mode '(c-mode))
+              (when (member major-mode '(c-mode c++-mode))
                 (clang-format-buffer))))
   ;; (add-hook 'c-mode-hook
   ;;           (function
