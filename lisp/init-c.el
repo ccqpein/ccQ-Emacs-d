e3a9d51b2f752c3a623d2c414a79d367ad8e300e
ccQpein <ccqpein@protonmail.com>
2020-11-06 00:15:29 -0500

    a bit

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,14 +1,25 @@
+(defun gcc-version ()
+  (car (last
+        (split-string
+         (shell-command-to-string "gcc-10 --version | head -n 1 | tr -d '\n'")
+         " "))))
+
 (use-package cc-mode
   :hook
   (cc-mode . clang-format)
 
   :config
+  (setq gcc-version (gcc-version))
   (setq clang-format-style "file"
         clang-format-fallback-style "google"
         flycheck-c/c++-gcc-executable "gcc-10"
-        flycheck-gcc-include-path '("/usr/include"
-                                    "/usr/local/Cellar/gcc/10.2.0/lib/gcc/10/gcc/x86_64-apple-darwin19/10.2.0/include/")
-        )
+        flycheck-gcc-include-path (list (format "/usr/local/Cellar/gcc/%s/include/c++/%s" gcc-version gcc-version)
+                                        (format "/usr/local/Cellar/gcc/%s/lib/include/c++/%s/x86_64-apple-darwin19"gcc-version gcc-version )
+                                        (format "/usr/local/Cellar/gcc/%s/include/c++/%s/backward"gcc-version gcc-version )
+                                        (format "/usr/local/Cellar/gcc/%s/lib/gcc/10/gcc/x86_64-apple-darwin19/%s/include"gcc-version gcc-version )
+                                        (format "/usr/local/Cellar/gcc/%s/lib/gcc/10/gcc/x86_64-apple-darwin19/%s/include-fixed"gcc-version gcc-version )
+                                        ))
+  (setq-default flycheck-disabled-checkers '(c/c++-clang))
   (add-hook 'before-save-hook
             (lambda ()
               (when (member major-mode '(c-mode c++-mode))
