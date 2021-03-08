b1d6419131557a1f72d5c521eca18c01b6c0d160
ccQpein <ccqpein@protonmail.com>
2021-03-08 12:29:17 -0500

    a bit

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -32,7 +32,9 @@
                          (lsp-organize-imports)
                          (lsp-format-buffer))))
   (setq lsp-rust-analyzer-diagnostics-disabled (vector "macro-error")
-        lsp-rust-analyzer-proc-macro-enable t)
+        lsp-rust-analyzer-proc-macro-enable t
+        lsp-rust-analyzer-cargo-load-out-dirs-from-check t
+        )
 ;;;:= TODO: bind commands lsp-rust-analyzer-join-lines, lsp-extend-selection and lsp-rust-analyzer-expand-macro
   )
 
