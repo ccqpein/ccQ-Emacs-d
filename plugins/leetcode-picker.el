a5ad95955b096d95c4afe055b55702b9b86c25d0
ccQpein <ccqpein@protonmail.com>
2021-06-29 14:37:08 -0400

    new mode

--- plugins/leetcode-picker.el
+++ plugins/leetcode-picker.el
@@ -1,18 +1,38 @@
 (require 'leetcode-picker-inner)
 
-(defun get-random-quiz (fmt-str)
+(defun get-random-lc-quiz (fmt-str)
   (interactive "sImport your format (empty using default): ")
-  (cond ((string= "" fmt-str)
-         (leetcode-picker-inner-get-random-with-level level "#README# {title}\n\n{content}"))
-        (t (leetcode-picker-inner-get-random fmt-str))))
+  (get-buffer-create "quiz-description")
+  (with-current-buffer "quiz-description"
+    (markdown-mode)
+    (kill-region (point-min) (point-max))
+    (insert
+     (cond ((string= "" fmt-str)
+            (leetcode-picker-inner-get-random "#README# {title}\n\n{content}"))
+           (t (leetcode-picker-inner-get-random fmt-str))))))
 
-(defun get-random-quiz-with-level (fmt-str level)
+(defun get-random-lc-quiz-with-level (fmt-str level)
   (interactive "sImport your format (empty using default): \nsLevel of quiz (e/m(default)/h): ")
-  (cond ((string= "" fmt-str)
-         (leetcode-picker-inner-get-random-with-level level "#README# {title}\n\n{content}"))
-        (t (leetcode-picker-inner-get-random-with-level level fmt-str)))
-  )
+  (get-buffer-create "quiz-description")
+  (with-current-buffer "quiz-description"
+    (markdown-mode)
+    (kill-region (point-min) (point-max))
+    (insert
+     (cond ((string= "" fmt-str)
+            (leetcode-picker-inner-get-random-with-level
+             level
+             "#README# {title}\n\n{content}"))
+           (t (leetcode-picker-inner-get-random-with-level level fmt-str))))))
 
-;;:= TODO: not finish yet
-;;(provide 'leetcode-picker)
+(defun get-lc-quiz-snippet (lang)
+  "get quiz snippet"
+  (interactive "sImport your language (empty using rust): ")
+  (get-buffer-create "code-snippet")
+  (with-current-buffer "code-snippet"
+    (kill-region (point-min) (point-max))
+    (insert
+     (cond ((string= "" lang)
+            (leetcode-picker-inner-get-code-snippet "rust"))
+           (t (leetcode-picker-inner-get-code-snippet lang))))))
 
+(provide 'leetcode-picker)
