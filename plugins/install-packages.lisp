818f55b62e516dc5550f7c73e77774b7f942bc42
ccQpein <ccqpein@protonmail.com>
2023-01-18 15:14:26 -0500

    update

--- plugins/install-packages.lisp
+++ plugins/install-packages.lisp
@@ -4,13 +4,16 @@
 	("ejira" "https://github.com/nyyManni/ejira.git" "~/.emacs.d/elpa/")
 	))
 
+(defun op-path (path)
+  (uiop:parse-unix-namestring (truename path)))
+
 (defun check-if-already-install (git-repo-info)
   (let ((current-dir (sb-posix:getcwd))
 		(install-path (third git-repo-info))
 		(folder-name (first git-repo-info))
 		(stdout (make-string-output-stream)))
 	
-	(sb-posix:chdir install-path)
+	(sb-posix:chdir (op-path install-path))
 	(probe-file (concatenate 'string install-path folder-name))
 	))
 
@@ -20,19 +23,29 @@
 		(folder-name (first git-repo-info))
 		(stdout (make-string-output-stream)))
 	
-	(sb-posix:chdir install-path)
-	(sb-ext:run-program "git" (list "clone" (second git-repo-info))
+	(sb-posix:chdir (op-path install-path))
+	(sb-ext:run-program "git" (list "clone" (second git-repo-info) folder-name)
 						:search t
                         :output stdout)
 	(format t "~a~%" (get-output-stream-string stdout))
 	))
 
-(defun git-pull (git-repo-info))
+(defun git-pull (git-repo-info)
+  (let ((current-dir (sb-posix:getcwd))
+		(install-path (third git-repo-info))
+		(folder-name (first git-repo-info))
+		(stdout (make-string-output-stream)))
+	
+	(sb-posix:chdir (op-path (concatenate 'string install-path folder-name)))
+	(sb-ext:run-program "git" (list "pull")
+						:search t
+                        :output stdout)
+	(format t "~a~%" (get-output-stream-string stdout))
+	)
+  )
 
 (defun main ()
   (dolist (pkg *WILD-CARDS-PKGS*)
 	(if (check-if-already-install pkg)
 		(git-pull pkg)
 		(git-clone pkg))))
-
-(format t "~a~%" (sb-posix:getcwd))
