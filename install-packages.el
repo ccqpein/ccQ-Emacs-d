e54e727ec4654db8f1d5ac1d18876d38f3503da6
ccQpein <ccqpein@protonmail.com>
2023-01-21 20:46:39 -0500

    change install script

--- install-packages.el
+++ install-packages.el
@@ -16,24 +16,35 @@
 (package-initialize)
 
 ;;; read package list from `packages` file
-(defvar *packages-list (with-temp-buffer
-                         (insert-file-contents (concat cpath "packages"))
-                         (reverse (split-string (buffer-string) "\n" t))))
+(defvar *packages-list
+  (with-temp-buffer
+	(insert-file-contents (concat cpath "init.el"))
+	(cl-loop 
+	 for exp = (read (current-buffer))
+	 if (eql 'custom-set-variables (car exp))
+	 return (cl-loop
+			 for e in exp
+			 when (and (eql 'cons (type-of e))
+					   (eql 'package-selected-packages (caadr e)))
+			 return (cadr (cadadr e))
+			 ))))
 
-;(print *packages-list)
+;; (print *packages-list)
 ;; refrash contents anyway in case cannot found package
 (package-refresh-contents)
 
 ;;; install packages
 (dolist (package *packages-list)
-  (if (package-installed-p (intern package))
+  (if (package-installed-p package)
       (princ (format "%s already installed \n" package))
     (progn
       (princ (format "%s is installing \n" package))
-      (package-install (intern package))
+      (package-install package)
       (princ (format "%s installed \n\n" package)))))
 
 ;;; run the addtional scripts if there is one
-(let ((default-directory (concat cpath "plugins")))
-  (princ (format "starting to run additional script"))
-  (princ (shell-command-to-string "sbcl --load install-packages.lisp --non-interactive")))
+(if (yes-or-no-p "wanna run addtional package installment? ")
+	(let ((default-directory (concat cpath "plugins")))
+	  (princ (format "starting to run additional script"))
+	  (princ (shell-command-to-string "sbcl --load install-packages.lisp --non-interactive"))))
+
