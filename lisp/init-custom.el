b56b20bb6b1b55a55a6a1b89ef23bd260f3658f1
ccQpein <ccqpein@protonmail.com>
2023-02-06 22:08:19 -0500

    looks like rust-ts-mode somehow become the primary

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -7,6 +7,7 @@
   ;;(add-hook 'go-mode-hook 'go-ts-mode)
   ;;(add-hook 'python-mode-hook 'python-ts-mode)
   ;;(add-hook 'rust-mode-hook 'rust-ts-mode)
+  (add-hook 'rust-ts-mode-hook 'rust-mode)
 
 
   ;; for dash
