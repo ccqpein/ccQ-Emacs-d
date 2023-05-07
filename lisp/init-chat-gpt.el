625ac093515fad5dc58e6ead55a2c62dff05238f
ccQpein <ccqpein@protonmail.com>
2023-05-07 20:48:25 +0800

    Looks good for now

--- lisp/init-chat-gpt.el
+++ lisp/init-chat-gpt.el
@@ -1,8 +1,8 @@
 (use-package chatgpt-shell
   :straight (chatgpt-shell :type git :host github :repo "xenodium/chatgpt-shell")
   :custom
-  ((chatgpt-shell-openai-key
-    (lambda ()
-      (exec-path-from-shell-copy-env "OPENAI_API_KEY")))))
+  ((chatgpt-shell-model-version "gpt-4")
+   (chatgpt-shell-openai-key
+    (exec-path-from-shell-copy-env "OPENAI_API_KEY"))))
 
 (provide 'init-chat-gpt)
