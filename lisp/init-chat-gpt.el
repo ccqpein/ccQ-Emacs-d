490b647594bee2c2ac6867b4dbc814c45d882ab5
ccQpein <ccqpein@protonmail.com>
2023-05-31 20:35:38 -0400

    update chatgpt

--- lisp/init-chat-gpt.el
+++ lisp/init-chat-gpt.el
@@ -1,8 +1,13 @@
+(use-package shell-maker
+  :straight (:host github :repo "xenodium/chatgpt-shell" :files ("shell-maker.el")))
+
 (use-package chatgpt-shell
-  :straight (chatgpt-shell :type git :host github :repo "xenodium/chatgpt-shell")
+  :requires shell-maker
+  :straight (:host github :repo "xenodium/chatgpt-shell" :files ("chatgpt-shell.el"))
+  
   :custom
-  ((chatgpt-shell-model-version "gpt-4")
-   (chatgpt-shell-openai-key
-    (exec-path-from-shell-copy-env "OPENAI_API_KEY"))))
+  (chatgpt-shell-model-version "gpt-4")
+  (chatgpt-shell-openai-key
+   (exec-path-from-shell-copy-env "OPENAI_API_KEY")))
 
 (provide 'init-chat-gpt)
