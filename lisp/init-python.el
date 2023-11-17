f8930f09720f8d1d6dd5cffdd11a53eabdcddbec
ccQpein <ccqpein@protonmail.com>
2023-11-17 18:29:26 -0500

    no need flymake

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,6 +2,7 @@
 (use-package elpy
   :init
   (elpy-enable)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
 
   ;; for ts mode, has to add the hook here 2023/11/11
   :hook (python-ts-mode . elpy-mode)
@@ -20,8 +21,7 @@
   (straight-use-package 'py-autopep8)
   (setq lsp-pylsp-plugins-yapf-enabled     t
         lsp-pylsp-plugins-autopep8-enabled t
-        lsp-pylsp-plugins-flake8-enabled   t)
-  )
+        lsp-pylsp-plugins-flake8-enabled   t))
 
 (provide 'init-python)
 
