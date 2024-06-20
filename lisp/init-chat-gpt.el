534f18fbfd0bbac0dda8906145a5e53c457ed503
ccQpein <ccqpein@protonmail.com>
2024-06-20 11:24:12 -0400

    fix

--- lisp/init-chat-gpt.el
+++ lisp/init-chat-gpt.el
@@ -1,5 +1,7 @@
 (use-package shell-maker
-  :straight (:host github :repo "xenodium/chatgpt-shell" :files ("shell-maker.el")))
+  :straight (:host github :repo "xenodium/chatgpt-shell" :files ("shell-maker.el"))
+  :custom
+  (shell-maker-transcript-default-path "~/Desktop/Org/GPT-transcript"))
 
 (use-package chatgpt-shell
   :straight (:host github :repo "xenodium/chatgpt-shell" :files ("chatgpt-shell.el"))
@@ -8,18 +10,20 @@
   (chatgpt-shell-model-version "gpt-4o")
   (chatgpt-shell-openai-key
    (exec-path-from-shell-copy-env "OPENAI_API_KEY"))
-  (chatgpt-shell-system-prompts
-   (delete-dups
-    (append '(("nil" . "")
-              ("Grammar/Spell checker" . "You are going to correct the grammar and spelling. Try you best to keep the languages style.")
-              ("Writing helper" . "Your task is to correct the grammar and spelling of various articles. These could include programming documents, READMEs, technical documents, papers, reports, and more. Please ensure you maintain the author's personal writing style appropriate to each type of article."))
-            chatgpt-shell-system-prompts)))
+  
   ;; use nil as the default
   (chatgpt-shell-system-prompt 0)
 
   :bind
   (:map chatgpt-shell-mode-map
         ("M-<return>" . chatgpt-shell-submit)
-        ("<return>" . chatgpt-shell-newline)))
+        ("<return>" . chatgpt-shell-newline))
+
+  :config
+  (setf chatgpt-shell-system-prompts
+        (append '(("nil" . "")
+                  ("Grammar/Spell checker" . "You are going to correct the grammar and spelling. Try you best to keep the languages style.")
+                  ("Writing helper" . "Your task is to correct the grammar and spelling of various articles. These could include programming documents, READMEs, technical documents, papers, reports, and more. Please ensure you maintain the author's personal writing style appropriate to each type of article."))
+                chatgpt-shell-system-prompts)))
 
 (provide 'init-chat-gpt)
