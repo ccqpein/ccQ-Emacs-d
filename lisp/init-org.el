18399052aee11715f73221945bdae2f4d66d7c5d
ccQpein <ccqpein@protonmail.com>
2024-12-02 00:55:19 -0500

    update org mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -20,7 +20,7 @@
   (org-directory (getenv "ORG_PATH"))
   (org-log-done t)
   (org-hide-leading-stars t)
-  (org-hide-emphasis-markers t)
+  ;;(org-hide-emphasis-markers t)
   (org-log-into-drawer t)
   (org-babel-load-languages
    '((emacs-lisp . t)
@@ -28,8 +28,8 @@
      (python . t)
      (lisp . t)
      (shell . t)))
-  
-  ;;; capture templates
+
+  ;; capture templates
   (org-capture-templates
    '(("t" "Header template version 0" plain (function buffer-file-name)
       (file
@@ -38,6 +38,25 @@
   
   ;; define the location of archive
   (org-archive-location "archive/%s_archive::* Archived Tasks")
-  )
+
+  :config
+  ;; my emphasis function
+  (defun toggle-emphasis ()
+    (interactive)
+    (save-match-data
+      (let* ((element (org-element-context))
+             (type (org-element-type element))
+             (pos (point)))
+        (if (or (eq type 'code))
+            (progn
+              (goto-char (1- (org-element-property :end element)))
+              (delete-char 1)
+              (goto-char (org-element-property :begin element))
+              (delete-char 1)
+              (goto-char (1- pos))
+              )
+          (call-interactively #'org-emphasize)))))
+  
+  (global-set-key [remap org-emphasize] #'toggle-emphasis))
 
 (provide 'init-org)
