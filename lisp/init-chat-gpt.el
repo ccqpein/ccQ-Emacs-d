fa63edcde72bf35f2803cca13a3d84e16df942bd
ccQpein <ccqpein@protonmail.com>
2025-02-01 23:02:43 -0500

    lets go google!

--- lisp/init-chat-gpt.el
+++ lisp/init-chat-gpt.el
@@ -20,6 +20,7 @@
   
   ;; use nil as the default
   (chatgpt-shell-system-prompt nil)
+  (chatgpt-shell-model-version "gemini-2.0-flash-exp")
 
   :bind
   (:map chatgpt-shell-mode-map
@@ -28,6 +29,20 @@
         ("C-c M-o" . chatgpt-shell-clear-buffer))
 
   :config
+  (push
+   (chatgpt-shell-openai-make-model
+    :version "o3-mini"
+    :token-width 3
+    :context-window 200000
+    :validate-command
+    ;; TODO: Standardize whether or not a model supports system prompts.
+    (lambda (command model settings)
+      (or (chatgpt-shell-openai--validate-command command model settings)
+          (when (map-elt settings :system-prompt)
+            (format "Model \"%s\" does not support system prompts. Please unset via \"M-x chatgpt-shell-swap-system-prompt\" by selecting None."
+                    (map-elt model :version))))))
+   chatgpt-shell-models)
+  
   (setf chatgpt-shell-system-prompts
         (delete-dups
          (append '(("Grammar/Spell checker" . "You are going to correct the grammar and spelling. Try you best to keep the languages style.")
