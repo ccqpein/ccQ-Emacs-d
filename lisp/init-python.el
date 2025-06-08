10a6dd95ac58d652ca142919c892bb7794e5f0ca
ccQpein <ccqpein@protonmail.com>
2025-06-08 14:29:12 -0400

    fix some elpy error

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -11,7 +11,6 @@
   (python-shell-completion-native-enable nil)
   (python-shell-interpreter "uv")
   (python-shell-interpreter-args "run ipython --simple-prompt -c exec('__import__(\\'readline\\')') -i --pprint")
-  (elpy-rpc-python-command "~/.pyenv/shims/python")
   
   :bind
   (:map elpy-mode-map
@@ -19,6 +18,9 @@
   
   :config
   (straight-use-package 'py-autopep8)
+  (setenv "PYTHONIOENCODING" "utf-8")
+  (add-to-list 'process-coding-system-alist '("python" . (utf-8 . utf-8)))
+  (add-to-list 'process-coding-system-alist '("elpy" . (utf-8 . utf-8)))
   (setq lsp-ruff-lsp-show-notifications "onError"))
 
 (provide 'init-python)
