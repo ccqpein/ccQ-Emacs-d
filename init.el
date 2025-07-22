892f0182ef1b552a48995d4b1241dd461174ab58
ccQpein <ccqpein@protonmail.com>
2025-07-21 23:48:00 -0400

    default height

--- init.el
+++ init.el
@@ -147,6 +147,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 140))))
  '(lsp-face-highlight-read ((t (:inherit highlight :background "gray27"))))
  '(lsp-face-highlight-textual ((t (:inherit highlight :background "gray27"))))
  '(lsp-face-highlight-write ((t (:inherit highlight :background "gray27"))))
